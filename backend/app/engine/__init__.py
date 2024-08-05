3ad74eda68cc47251d99f0cd006ab7393c812b17
Santiago Marro <smarro@gmail.com>
2024-08-05 12:39:07 -0300

    Added Cohere reranker

--- backend/app/engine/__init__.py
+++ backend/app/engine/__init__.py
@@ -5,18 +5,31 @@ from llama_index.core.tools.query_engine import QueryEngineTool
 from app.engine.index import get_index
 from llama_index.core.tools import ToolMetadata
 from llama_index.tools.wikipedia import WikipediaToolSpec
+from llama_index.postprocessor.cohere_rerank import CohereRerank
+
+
+cohere_rerank = None
 
 
 def get_chat_engine(filters=None):
+    # Reranker
+    global cohere_rerank
+    if not cohere_rerank:
+        api_key = os.getenv("COHERE_API_KEY")
+        top_k_ranked = os.getenv("TOP_K_RANKED", "3")
+        cohere_rerank = CohereRerank(api_key=api_key, top_n=top_k_ranked)
+
     system_prompt = os.getenv("SYSTEM_PROMPT")
-    top_k = os.getenv("TOP_K", "3")
+    top_k = os.getenv("TOP_K", "10")
     tools = []
 
     # Add query tool if index exists
     index = get_index()
     if index is not None:
         query_engine = index.as_query_engine(
-            similarity_top_k=int(top_k), filters=filters
+            similarity_top_k=int(top_k),
+            filters=filters,
+            node_postprocessors=[cohere_rerank],
         )
         query_engine_tool = QueryEngineTool(
             query_engine=query_engine,
